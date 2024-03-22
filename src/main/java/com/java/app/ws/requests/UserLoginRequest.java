ad4c932a29a3e50f58b74ae76c1617f7ea7a3eda
springbootproje <ilhambouhout2001@gmail.com>
2024-03-23 00:07:30 +0100

    adding autho

---
+++ src/main/java/com/java/app/ws/requests/UserLoginRequest.java
@@ -0,0 +1,19 @@
+package com.java.app.ws.requests;
+
+public class UserLoginRequest {
+private String email;
+private String passsword;
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPasssword() {
+	return passsword;
+}
+public void setPasssword(String passsword) {
+	this.passsword = passsword;
+}
+
+}
--- src/main/java/com/java/app/ws/security/webSecurity.java
+++ src/main/java/com/java/app/ws/security/webSecurity.java
@@ -6,24 +6,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+
 @EnableWebSecurity
 public class webSecurity {
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
-        .authorizeRequests(authConfig -> {
-        	
-        
-            authConfig.antMatchers("/").permitAll();
-            authConfig.antMatchers("/users/**").authenticated();
-            authConfig.antMatchers("/admin/**").denyAll();
-            
-            })
-        .formLogin(customizer.withDefaults())
-        .httpBasic(customizer.withDefaults());
-           
-        return http.build();
-        
+            .cors().and()
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers(HttpMethod.POST, "/users")
+            .permitAll()
+            .anyRequest().authenticated();
+    }
 }
-}
+
+	
