647ff0576eed526372b8b393d5fc30740b3f0ddf
ilham_z463yxl <ilhambouhout2001@gmail.com>
2024-06-11 21:44:27 +0200

    front/backend

--- src/main/java/com/java/app/ws/security/CorsConfig.java
+++ src/main/java/com/java/app/ws/security/CorsConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.List;
-
+//hhhh
 @Configuration
 public class CorsConfig {
 
